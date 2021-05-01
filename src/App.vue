2c5d1e1adff01fc3c5bd12dadd941b2f98cde81a
jkyyyy <https://github.com/jkyyyy/supermail1.git>
2021-05-01 12:28:21 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+<div>
+  <h2>{{message}}</h2>
+</div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  components: {
+
+  },
+  data() {
+    return {
+      message: 88
+    }
+  }
+}
+</script>
+
+<style>
+@import "assets/css/base.css";
+</style>
